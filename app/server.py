7a69f072ca400a5f80a2786260b5408f28e3737b
Ambartsumyan Karen <kar.amb@yandex.ru>
2018-03-31 16:25:03 +0300

    added password check

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 my_flask_app = Flask(__name__)
 
@@ -9,8 +9,17 @@ def index():
 
 
 @my_flask_app.route('/registration/')
-def all_news():
+def registration():
     return render_template('registration.html')
+
+@my_flask_app.route('/add_user/', methods=['POST'])
+def add_user():
+    print(request.form)
+    password = request.form.get('password')
+    confirm_password = request.form.get('confrim_password')
+    if password != confirm_password:
+        return  render_template('index.html')
+    return jsonify({'result' : 'ok'})
 
 
 if __name__ == '__main__':
