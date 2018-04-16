e8814ed2c60fd9bc2c1beb35814d0b9437413d16
Ambartsumyan Karen <kar.amb@yandex.ru>
2018-04-17 00:46:50 +0300

    Corrected database error with user_project table

--- app/server.py
+++ app/server.py
@@ -51,8 +51,10 @@ def create_project():
     user = current_user
     project_name = request.form.get("project_name")
     project = Project(project_name)
-    project_user = ProjectUser(user.id, project_name)
     db_session.add(project)
+    db_session.commit()
+    project_id = project.id
+    project_user = ProjectUser(user.id, project_id)
     db_session.add(project_user)
     db_session.commit()
     dict_of_projects = {}
@@ -67,12 +69,29 @@ def create_project():
                            user_role = user.role_id,
                            dict_of_projects=dict_of_projects)
 
+@my_flask_app.route("/create_stage", methods=['GET', 'POST'])
+@login_required
+def create_stage():
+    user = current_user
+    project_id = request.args.get("project_id")
+    dict_of_projects = {}
+    projects_for_user = ProjectUser.query.filter(ProjectUser.user_id == user.id)
+    for proj in projects_for_user:
+        p = Project.query.filter_by(id=proj.id)
+        for pr in p:
+            dict_of_projects[pr.id] = pr.name
+    print(project_id)
+    return render_template("menu_manager.html",
+                           user_nickname=user.nickname,
+                           user_role=user.role_id,
+                           dict_of_projects=dict_of_projects)
+
+
 @my_flask_app.route("/show_stages", methods=['GET', 'POST'])
 @login_required
 def show_stages():
     user = current_user
     project_id = request.args.get("project_id")
-
     projects_for_user = ProjectUser.query.filter(ProjectUser.user_id==user.id)
     dict_of_projects = {}
     for proj in projects_for_user:
@@ -82,7 +101,8 @@ def show_stages():
     return render_template("menu_manager.html",
                            user_nickname = user.nickname,
                            user_role = user.role_id,
-                           dict_of_projects=dict_of_projects)
+                           dict_of_projects=dict_of_projects,
+                           create_stage_button=True)
 
 
 @login_manager.user_loader
